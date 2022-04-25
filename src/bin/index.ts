4c9b068c0b7e91e856f3e7275a0f628bd49054bf
softspheredev <softspheredev@gmail.com>
2022-04-25 18:37:02 +0300

    DAE converter binaries

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -2,12 +2,15 @@ import path from 'path'
 
 export default {
   Darwin: {
-    fbx: path.join(__dirname, 'Darwin', 'fbx')
+    fbx: path.join(__dirname, 'Darwin', 'fbx'),
+    dae: path.join(__dirname, 'Darwin', 'dae')
   },
   Linux: {
-    fbx: path.join(__dirname, 'Linux', 'fbx')
+    fbx: path.join(__dirname, 'Linux', 'fbx'),
+    dae: path.join(__dirname, 'Linux', 'dae')
   },
   Windows_NT: {
-    fbx: path.join(__dirname, 'Windows_NT', 'fbx.exe')
+    fbx: path.join(__dirname, 'Windows_NT', 'fbx.exe'),
+    dae: path.join(__dirname, 'Windows_NT', 'dae.exe')
   }
 }
