fcc9163f86f1633f64123123f9808a70021d5c3a
softspheredev <softspheredev@gmail.com>
2022-04-26 06:23:48 +0300

    DAE converter implemented

--- src/converter/dae.ts
+++ src/converter/dae.ts
@@ -1,3 +1,16 @@
 import { Converter } from '@/types'
+import os from 'os'
+import bin from '@/bin'
+import fs from 'fs-extra'
+import { convertBase } from './convert-base'
+import { spawn } from 'child_process'
 
-export const dae: Converter = async (file) => file // TODO: Implement DAE converter
+export const dae: Converter = async (file) => {
+  const OS = os.type() as 'Darwin' | 'Windows_NT' | 'Linux'
+  const tool = bin[OS].dae
+  await fs.chmod(tool, 0o755)
+  return await convertBase(file, async (origin, destination) => {
+    const childProcess = spawn(tool, ['-b', origin, '-o', destination])
+    await new Promise<void>((resolve) => childProcess.on('close', () => resolve()))
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra'
 import converter from './converter'
 
-const file = fs.readFileSync('/Users/mihailpraslov/Desktop/original.fbx')
+const file = fs.readFileSync('/Users/mihailpraslov/Desktop/original.dae')
 
 converter(file)
   .then((glb) => {
