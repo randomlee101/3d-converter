a8fa9514f838eb9171d5c18cd12f2f775f3cb230
softspheredev <softspheredev@gmail.com>
2022-04-27 07:03:09 +0300

    Index exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import fs from 'fs-extra'
-import converter from './converter'
-
-const acc = fs.readFileSync('/Users/mihailpraslov/Downloads/retest/obj/acc.zip')
-const file = fs.readFileSync('/Users/mihailpraslov/Downloads/retest/obj/model.obj')
-
-converter(file, acc)
-  .then((glb) => {
-    return fs.writeFile('/Users/mihailpraslov/Downloads/retest/obj/model.glb', glb)
-  })
+export { default as convertToGlb } from './converter'
+export { default as detectFormat } from './detector'
